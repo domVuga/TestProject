87f4604d29ce61c11b0b352a138c779eff959113
DominicVuga <92914444+DominicVuga@users.noreply.github.com>
2022-12-21 11:39:19 +0100

    First commit

---
+++ datadisplay/datadisplay/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿
+namespace datadisplay
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGrid = new System.Windows.Forms.DataGridView();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.readButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(350, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Display";
+            // 
+            // dataGrid
+            // 
+            this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid.Location = new System.Drawing.Point(12, 29);
+            this.dataGrid.Name = "dataGrid";
+            this.dataGrid.RowHeadersWidth = 51;
+            this.dataGrid.RowTemplate.Height = 24;
+            this.dataGrid.Size = new System.Drawing.Size(776, 357);
+            this.dataGrid.TabIndex = 3;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(494, 404);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(82, 33);
+            this.buttonSave.TabIndex = 4;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // readButton
+            // 
+            this.readButton.Location = new System.Drawing.Point(176, 404);
+            this.readButton.Name = "readButton";
+            this.readButton.Size = new System.Drawing.Size(76, 33);
+            this.readButton.TabIndex = 5;
+            this.readButton.Text = "Read";
+            this.readButton.UseVisualStyleBackColor = true;
+            this.readButton.Click += new System.EventHandler(this.readButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.readButton);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.dataGrid);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGrid;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button readButton;
+    }
+}
+
---
+++ datadisplay/datadisplay/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClosedXML.Excel;
+using Excel2 = Microsoft.Office.Interop.Excel;
+using ExcelDataReader;
+
+
+
+namespace datadisplay
+{
+    public partial class Form1 : Form
+    {
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void readButton_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog dialog = new OpenFileDialog()
+            { Filter = "Excel workbook|*.xlsx", Multiselect = false })
+            {
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Define the datatable to store the data from excel
+                    DataTable dt = new DataTable();
+
+                    using (XLWorkbook workbook = new XLWorkbook(dialog.FileName))
+                    {
+                        bool IsFirstRow = true;     //Here we check we are in the process of writing the header
+                        var rows = workbook.Worksheet(3).RowsUsed();    //Choosing sheet 3 from the workbook
+                        foreach (var row in rows)
+                        {
+                            if (IsFirstRow)
+                            {
+                                //adding columns
+                                foreach (IXLCell cell in row.Cells())
+                                {
+                                    dt.Columns.Add(cell.Value.ToString());
+                                }
+                                IsFirstRow = false;
+                            }
+                            else
+                            {
+                                dt.Rows.Add();
+                                int i = 0;
+                                foreach (IXLCell cell in row.Cells())
+                                    dt.Rows[dt.Rows.Count - 1][i++] = cell.Value.ToString();
+                            }
+                        }
+                        //Show data on data grid view
+                        dataGrid.DataSource = dt;
+
+                    }
+
+                }
+            }
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            Excel2._Application app = new Excel2.Application();
+            Excel2._Workbook workbook = app.Workbooks.Add(Type.Missing);
+            Excel2._Worksheet worksheet = null;
+
+            app.Visible = true;
+            worksheet = workbook.Sheets["Sheet1"];
+            worksheet = workbook.ActiveSheet;
+            worksheet.Name = "EditSheet1";
+            
+
+            for (int i = 1; i < dataGrid.Columns.Count + 1; i++)
+            {
+                worksheet.Cells[1,i] = dataGrid.Columns[i-1].HeaderText;
+            }
+
+            for (int i = 1; i < dataGrid.Rows.Count - 1; i++)
+            {
+                for (int j = 1; j < dataGrid.Columns.Count; j++)
+                {
+                    worksheet.Cells[i+1, j] = dataGrid.Rows[i-1].Cells[j-1].Value.ToString();
+                }
+            }
+            workbook.SaveAs("C:\\Users\\Dominic Vuga\\Desktop\\myVSprojects\\datadisplay\\test.csv");
+        }
+    }
+}
